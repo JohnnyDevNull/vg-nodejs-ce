cb745821286847805cde3657722b2d7ad6e0ceec
Philipp John <philipp.john@live.de>
2024-02-24 22:40:19 +0100

    feat: update prisma dev seeding & encrypt password

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import { hashSync } from 'bcryptjs';
 
 import { customers } from './seeds/customers';
 
@@ -6,6 +7,7 @@ const prisma = new PrismaClient();
 
 async function main() {
   for (const customer of customers) {
+    customer.password = hashSync(customer.password, 10);
     await prisma.customer.upsert({
       where: { id: customer.id },
       update: {},
--- prisma/seeds/customers.ts
+++ prisma/seeds/customers.ts
@@ -4,11 +4,25 @@ export const customers: Prisma.CustomerUpsertArgs['create'][] = [
   {
     id: '9e391faf-64b2-4d4c-b879-463532920fd3',
     email: 'user@gmail.com',
+    role: 0,
+    activated: false,
+    activationCode: '123456',
     password: 'randow-password',
   },
   {
     id: '9e391faf-64b2-4d4c-b879-463532920fd4',
     email: 'user2@gmail.com',
+    role: 1,
+    activated: true,
+    activationCode: '654321',
     password: 'randow-password',
+  },
+  {
+    id: '9e391faf-64b2-4d4c-b879-463532920fd5',
+    email: 'admin1@gmail.com',
+    role: 2,
+    activated: true,
+    activationCode: '321987',
+    password: 'randow-password-admin',
   },
 ];
